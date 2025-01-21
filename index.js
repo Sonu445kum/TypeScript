89dc8ca21e09e37f35a0b1a3ec8cc48655723895
Sonu445Kum <sonuroy1619@gmail.com>
2025-01-22 02:58:49 +0530

    add a.ts file

---
+++ index.js
@@ -0,0 +1,4 @@
+"use strict";
+const x = 10;
+let y = 20;
+console.log("The sum of " + x + " and " + y + " is " + x + y);
---
+++ index.ts
@@ -0,0 +1,3 @@
+const x:number=10;
+let y: number = 20;
+console.log( "The sum of "+x+" and "+y+" is "+x+y);
